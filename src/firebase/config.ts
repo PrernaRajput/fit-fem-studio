6b775367a7fed6db204434fbf8e4368c55407cc7
Prerna Rajput <rajputprerna06@gmail.com>
2025-11-04 00:40:17 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -12,4 +12,11 @@
 // You can also right-click the "firebase.json" file in the file explorer and select
 // "Change Firebase Project" from the context menu.
 
-export const firebaseConfig = null;
+export const firebaseConfig = {
+  "projectId": "studio-5133780745-130d2",
+  "appId": "1:1050052849106:web:00fa72fe47d6d4abfa6c0b",
+  "apiKey": "AIzaSyDcxZlsp42k0JMHu3tfUMpfluiE9FsYmEs",
+  "authDomain": "studio-5133780745-130d2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1050052849106"
+};
